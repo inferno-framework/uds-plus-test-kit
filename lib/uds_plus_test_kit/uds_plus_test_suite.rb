a2931e6221d6712080b0b462eb9c13edc3b434e4
Lucas Tucker <ltucker@leaporbit.com>
2023-03-24 12:53:56 -0400

    Added test kit basics

--- lib/uds_plus_test_kit/uds_plus_test_suite.rb
+++ lib/uds_plus_test_kit/uds_plus_test_suite.rb
@@ -1,1 +1,40 @@
-require_relative 'uds_plus_test_kit/uds_plus_test_suite'
+require 'inferno/dsl/oauth_credentials'
+
+module UDSPlusTestKit
+    class UDSPlusTestSuite < Inferno::UDSPlusTestSuite
+        title 'UDS+ Test Kit'
+        description %(
+            The UDS+ Test Kit tests systems for their conformance to the [UDS+
+            Implementation Guide](http://fhir.drajer.com/site/index.html#uds-plus-home-page).
+        )
+
+        version VERSION
+
+        def self.metadata
+            @metadata ||= YAML.load_file(File.join(__dir__, 'metadata.yml'), aliases: true)[:groups].map do |raw_metadata|
+                Generator::GroupMetadata.new(raw_metadata)
+            end
+        end
+    
+        validator do
+            url ENV.fetch('V120_VALIDATOR_URL', 'http://validator_service:4567')
+        end
+
+        id :uds_plus
+
+        input :url,
+            title: 'FHIR Endpoint',
+            description: 'URL of the FHIR endpoint'
+        input :smart_credentials,
+            title: 'OAuth Credentials',
+            type: :oauth_credentials,
+            optional: true
+
+        fhir_client do
+            url :url
+            oauth_credentials :smart_credentials
+        end
+
+        # Test Groups go here
+    end
+end
