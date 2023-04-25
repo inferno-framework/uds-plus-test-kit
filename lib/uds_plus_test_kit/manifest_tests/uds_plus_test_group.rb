429435290ad3b96d85cbdd1d9dd5a7e32977e78e
Lucas Tucker <ltucker@leaporbit.com>
2023-04-25 10:42:33 -0400

    Changed manifest title

--- lib/uds_plus_test_kit/manifest_tests/uds_plus_test_group.rb
+++ lib/uds_plus_test_kit/manifest_tests/uds_plus_test_group.rb
@@ -14,7 +14,7 @@ require_relative '../validate_sexual_orientation_test'
 
 module UDSPlusTestKit
     class UDSPlusTestGroup < Inferno::TestGroup
-        title 'Data Submitter Tests'
+        title 'Import Manifest Tests'
         id :uds_plus_test_group 
         description %(
             The tests below validate the import manifest produced by a UDS+ Data Submitter. 
