45eb63b829ed9467cf03b960bad13923476207e6
Lucas Tucker <ltucker@leaporbit.com>
2023-05-01 12:02:59 -0400

    Cleaned up code for submission

--- Gemfile
+++ Gemfile
@@ -8,6 +8,4 @@ group :development, :test do
   gem 'debug'
   gem 'foreman'
   gem 'rerun'
-  gem 'rack'
-  #gem 'smart_app_launch_test_kit', '~> 0.2.2'
 end
--- lib/uds_plus_test_kit/uds_plus_test_suite.rb
+++ lib/uds_plus_test_kit/uds_plus_test_suite.rb
@@ -1,5 +1,4 @@
 require 'inferno/dsl/oauth_credentials'
-#require 'smart_app_launch_test_kit'
 require_relative './ext/fhir_models'
 require_relative './version'
 require_relative './manifest_tests/uds_plus_test_group'
@@ -29,44 +28,7 @@ module UDSPlusTestKit
             end
         end
 
-        PROFILE = {
-            'SexualOrientation' => 'http://hl7.org/fhir/us/uds-plus/StructureDefinition/uds-plus-sexual-orientation-observation',
-            'ImportManifest' => 'http://hl7.org/fhir/us/uds-plus/StructureDefinition/uds-plus-import-manifest',
-            'Income' => 'http://hl7.org/fhir/us/uds-plus/StructureDefinition/uds-plus-income-observation',
-            'DeIdentifyData' => 'http://hl7.org/fhir/us/uds-plus/StructureDefinition/uds-plus-deidentify-data',
-            'Procedure' => 'http://hl7.org/fhir/us/uds-plus/StructureDefinition/uds-plus-procedure',
-            'Patient' => 'http://hl7.org/fhir/us/uds-plus/StructureDefinition/de-identified-uds-plus-patient',
-            'Encounter' => 'http://hl7.org/fhir/us/uds-plus/StructureDefinition/uds-plus-encounter',
-            'Coverage' => 'http://hl7.org/fhir/us/uds-plus/StructureDefinition/uds-plus-coverage',
-            'Condition' => 'http://hl7.org/fhir/us/uds-plus/StructureDefinition/uds-plus-diagnosis'
-        }
-
         id :uds_plus
-
-        # Example urls generated here
-        patient_ex = File.read(File.join(__dir__, 'examples/patient.ndjson'))
-        patient_ex_route_handler = proc { [200, { 'Content-Type' => 'application/ndjson' }, [patient_ex]] }
-        route(:get, "/examples/patient", patient_ex_route_handler)
-
-        condition_ex = File.read(File.join(__dir__, 'examples/condition.ndjson'))
-        condition_ex_route_handler = proc { [200, { 'Content-Type' => 'application/ndjson' }, [condition_ex]] }
-        route(:get, "/examples/condition", condition_ex_route_handler)
-
-        encounter_ex = File.read(File.join(__dir__, 'examples/encounter.ndjson'))
-        encounter_ex_route_handler = proc { [200, { 'Content-Type' => 'application/ndjson' }, [encounter_ex]] }
-        route(:get, "/examples/encounter", encounter_ex_route_handler)
-
-        manifest_ex = File.read(File.join(__dir__, 'examples/manifest.json'))
-        manifest_ex_route_handler = proc { [200, { 'Content-Type' => 'application/json' }, [manifest_ex]] }
-        route(:get, "/examples/manifest", manifest_ex_route_handler)
-        
-        bad_condition_ex = File.read(File.join(__dir__, 'examples/invalid_condition.ndjson'))
-        bad_condition_ex_route_handler = proc { [200, { 'Content-Type' => 'application/ndjson' }, [bad_condition_ex]] }
-        route(:get, "/examples/invalid_condition", bad_condition_ex_route_handler)
-
-        bad_patient_ex = File.read(File.join(__dir__, 'examples/invalid_patient.ndjson'))
-        bad_patient_ex_route_handler = proc { [200, { 'Content-Type' => 'application/ndjson' }, [bad_patient_ex]] }
-        route(:get, "/examples/invalid_patient", bad_patient_ex_route_handler)
 
         group from: :uds_plus_test_group
         group from: :uds_plus_resource_test_group
