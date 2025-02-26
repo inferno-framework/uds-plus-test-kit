d44bfb22dbf5bc6945efa68de5952dd82ae69186
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-02-26 17:05:45 -0500

    Bump version to 1.2.0 (#43)

--- lib/uds_plus_test_kit/version.rb
+++ lib/uds_plus_test_kit/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module UDSPlusTestKit
-  VERSION = '1.1.2'
+  VERSION = '1.2.0'
   UDS_PLUS_VERSION = '1.1.0'
-  LAST_UPDATED = '2025-30-1'
+  LAST_UPDATED = '2025-02-26'
 end
