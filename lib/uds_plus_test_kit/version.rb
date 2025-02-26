0ce69a035d496e9fe84c074254bfca5e9baa343f
Chris Dillon <45436454+chris-at-work@users.noreply.github.com>
2025-02-26 12:46:26 -0800

    Migrate to inferno-core 0.6.x and required changes such as metadata (#42)

--- lib/uds_plus_test_kit/version.rb
+++ lib/uds_plus_test_kit/version.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module UDSPlusTestKit
-    VERSION = '1.1.2'
-    UDS_PLUS_VERSION = '1.1.0'
+  VERSION = '1.1.2'
+  UDS_PLUS_VERSION = '1.1.0'
+  LAST_UPDATED = '2025-30-1'
 end
